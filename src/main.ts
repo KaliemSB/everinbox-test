7c43fef0ae3223a879ed43b64cbf5b69883a941d
KaliemSB <mycael590@gmail.com>
2025-08-21 01:02:03 -0300

    feat: added linter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "@/app.module";
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@/app.module';
 
 const app = await NestFactory.create(AppModule);
 const logger = new Logger(AppModule.name);
 const PORT = process.env.PORT ?? 3000;
 
 await app.listen(PORT, () => {
-	logger.log(`Server is listening at port ${PORT}`);
-	logger.log(`Current environment is: ${process.env.NODE_ENV}`);
+  logger.log(`Server is listening at port ${PORT}`);
+  logger.log(`Current environment is: ${process.env.NODE_ENV}`);
 });
